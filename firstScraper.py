42d5f168d23e7c061970374a83742191be842543
Andre Le <andre@andrele.com>
2014-02-07 20:19:38 -0500

    Now parses first level ingredients from recipes

--- firstScraper.py
+++ firstScraper.py
@@ -1,6 +1,9 @@
+import json
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
+
+f = open("recipeIngredients", "w")
 
 from bs4 import BeautifulSoup
 
@@ -10,7 +13,6 @@ for i in range(2,10):
 	url = "allrecipes.com/recipes/breakfast-and-brunch/main.aspx?evt19=1&Page="+str(i)+"&vm=l&p34=HR_ListView#recipes"
 
 	r = requests.get("http://" +url)
-
 	data = r.text
 
 	soup = BeautifulSoup(data)
@@ -21,5 +23,13 @@ for i in range(2,10):
 		if header != None:
 			link = header.a["href"]
 			print(link)
+			req = requests.get(link)
+			recipeData = req.text
+			recipePage = BeautifulSoup(recipeData)
+			name = recipePage.find(id="lblIngName")
+			while name != None:
+				print(name.string)
+				name = recipePage.find_next("span", id="lblIngName")
+
 		divs = divs.find_next("div", "hub-list-view")
 
